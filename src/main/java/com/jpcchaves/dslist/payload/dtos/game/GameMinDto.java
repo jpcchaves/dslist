dcdfafc28f4205a6fd6b7b6205b9dec0af0c7289
João Paulo <jpcchaves@outlook.com>
2023-05-09 14:09:31 -0300

    feat: add game min dto

---
+++ src/main/java/com/jpcchaves/dslist/payload/dtos/game/GameMinDto.java
@@ -0,0 +1,64 @@
+package com.jpcchaves.dslist.payload.dtos.game;
+
+public class GameMinDto {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDto() {
+    }
+
+    public GameMinDto(Long id,
+                      String title,
+                      Integer year,
+                      String imgUrl,
+                      String shortDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+}
