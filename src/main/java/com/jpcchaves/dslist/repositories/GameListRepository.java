e3d8a4539e42cb8cc8c6db5b84508e0719713420
João Paulo <jpcchaves@outlook.com>
2023-05-10 13:27:37 -0300

    feat: add gamelist repository

---
+++ src/main/java/com/jpcchaves/dslist/repositories/GameListRepository.java
@@ -0,0 +1,10 @@
+package com.jpcchaves.dslist.repositories;
+
+import com.jpcchaves.dslist.entities.Game;
+import com.jpcchaves.dslist.entities.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+}
