06bc864ec1c7f8ab41b3ebb088554b0db507b397
João Paulo <jpcchaves@outlook.com>
2023-05-09 22:40:12 -0300

    feat: add belonging, belongingPK classes and impl domain model with relations between tables

--- src/main/java/com/jpcchaves/dslist/entities/GameList.java
+++ src/main/java/com/jpcchaves/dslist/entities/GameList.java
@@ -1,6 +1,12 @@
 package com.jpcchaves.dslist.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_game_list")
 public class GameList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
